1f7888304c70f2376f942c83934ecaf69ba0fb54
Benish <bsarinelli@gmail.com>
2018-11-02 13:13:08 -0700

    My age in dog years calculator.

---
+++ dogyears.js
@@ -0,0 +1,12 @@
+// My age set to myAge variable.
+const myAge = 25;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+//console.log(myAge);
+console.log(earlyYears);
+//console.log(laterYear);
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'Toby'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
